81460f102fafedd222d93c5de9f8ba0433d0a2eb
Björn Tirsén <bjorntirsen@gmail.com>
2021-11-19 15:22:04 +0100

    :sparkles: Connect frontend form to auth backend

--- components/auth/auth-form.js
+++ components/auth/auth-form.js
@@ -1,4 +1,6 @@
 import { useState, useRef } from 'react';
+import { signIn } from 'next-auth/client';
+
 import classes from './auth-form.module.css';
 
 const createUser = async (email, password) => {
@@ -32,7 +34,12 @@ function AuthForm() {
     const enteredPassword = passwordInputRef.current.value;
     // add validation
     if (isLogin) {
-      // Login user
+      const result = await signIn('credentials', {
+        redirect: false,
+        email: enteredEmail,
+        password: enteredPassword,
+      });
+      console.log(result);
     } else {
       try {
         const result = await createUser(enteredEmail, enteredPassword);
